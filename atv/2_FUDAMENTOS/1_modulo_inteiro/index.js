d68de1d84be2a196c2c564457670e234ae3ce74e
Davi Fernandes <davifernandes2001@gmail.com>
2025-08-13 13:34:44 -0300

    exp,iprt

--- atv/2_FUDAMENTOS/1_modulo_inteiro/index.js
+++ atv/2_FUDAMENTOS/1_modulo_inteiro/index.js
@@ -1,4 +1,4 @@
-const meuModulo = require('./meu_modulo')
+const meuModulo = require('./meu_modulo') // obs : o ./ é importante , direciona o arquivo base 
 const soma = meuModulo.soma
 
 soma(2, 3)
---
+++ atv/2_FUDAMENTOS/2_export_import/index.mjs
@@ -0,0 +1,4 @@
+import soma from './meu_modulo.mjs'
+
+soma (10, 56)
+soma (43, 6)
