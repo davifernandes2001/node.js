387b397c2b3219c4fe98f1e02ff4f6e7b9e30c04
Davi Fernandes <davifernandes2001@gmail.com>
2025-08-17 17:27:30 -0300

    EXEconsole

---
+++ atv/2_FUDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,16 @@
+//mais de uma valor 
+const x = 10 
+const y = 'Apolo 11'
+const z = [1, 2]
+
+console.log(x, y, z)
+
+// contagem de impressões
+console.count(`o valor de x é: ${x}, contagem`)
+console.count(`o valor de x é: ${x}, contagem`)
+console.count(`o valor de x é: ${x}, contagem`)
+console.count(`o valor de x é: ${x}, contagem`)
+console.count(`o valor de x é: ${x}, contagem`)
+
+// variavel entre string
+console.log('o nome é %s, ele é uma foguete ',y)
